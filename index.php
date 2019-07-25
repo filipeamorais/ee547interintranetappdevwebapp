e9c2031017418bd653192230dc209f81f4ca5232
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-24 22:18:57 -0500

    new: mistake - now it is adding the sql query to the page.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
@@ -35,7 +36,7 @@
     <script language="javascript" type="text/javascript" src="scripts/slideitmoo-1.1.js"></script>
     <script language="javascript" type="text/javascript">
         window.addEvents({
-            'domready': function() {
+            'domready': function () {
                 /* thumbnails example , div containers */
                 new SlideItMoo({
                     overallContainer: 'SlideItMoo_outer',
@@ -87,8 +88,10 @@
 
                 <div id="templatemo_search">
                     <form action="#" method="get">
-                        <input type="text" value="Search" name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
-                        <input type="submit" name="Search" value="" alt="Search" id="searchbutton" title="Search" class="sub_btn" />
+                        <input type="text" value="Search" name="keyword" id="keyword" title="keyword"
+                            onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
+                        <input type="submit" name="Search" value="" alt="Search" id="searchbutton" title="Search"
+                            class="sub_btn" />
                     </form>
                 </div>
             </div>
@@ -126,7 +129,8 @@
         <div id="templatemo_middle">
             <img src="images/foodiesfeed.com_coffee-beans-in-a-leaf-macro.png" alt="coffee-beans-in-a-leaf-macro" />
             <h1>Introducing Roots Coffee</h1>
-            <p>Roots Coffee is an online coffee shop that brings only renowed coffee brands direct from local producers of the world bests coffee places.</p>
+            <p>Roots Coffee is an online coffee shop that brings only renowed coffee brands direct from local producers
+                of the world bests coffee places.</p>
             <a href="#" class="buy_now">Browse All Products</a>
         </div>
         <!-- END of middle -->
@@ -153,12 +157,16 @@
                     <li><a href="#">Tincidunt leo</a></li>
                     <li><a href="#">Vestibulum blandit</a></li>
                 </ul>
-                <h3><a class="sidebar_title" href="http://www.mystockvector.com" title="stock vector" target="_blank">Newsletter</a></h3>
+                <h3><a class="sidebar_title" href="http://www.mystockvector.com" title="stock vector"
+                        target="_blank">Newsletter</a></h3>
                 <p>Praesent aliquam mi id tellus pretium pulvinar in vel ligula.</p>
                 <div id="newsletter">
                     <form action="#" method="get">
-                        <input type="text" value="Subscribe" name="email_newsletter" id="email_newsletter" title="email_newsletter" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
-                        <input type="submit" name="subscribe" value="Subscribe" alt="Subscribe" id="subscribebtn" title="Subscribe" class="subscribebtn" />
+                        <input type="text" value="Subscribe" name="email_newsletter" id="email_newsletter"
+                            title="email_newsletter" onfocus="clearText(this)" onblur="clearText(this)"
+                            class="txt_field" />
+                        <input type="submit" name="subscribe" value="Subscribe" alt="Subscribe" id="subscribebtn"
+                            title="Subscribe" class="subscribebtn" />
                     </form>
                     <div class="cleaner"></div>
                 </div>
@@ -166,6 +174,16 @@
             <!-- END of sidebar -->
 
             <?php
+            include '../appdata/rootsdb.inc';
+
+            $connection = mysqli_connect($host, $user, $password, $database);
+            
+            if (mysqli_connect_errno()) {
+              die(mysqli_connect_error());    // not production error handling
+            }
+
+            $sqlSelectAllTheProducts = "SELECT * FROM tblproduct";
+            ?>
         <div id="content">
         	<div class="col col_14 product_gallery">
             	<a href="productdetail.html"><img src="images/product/01.jpg" alt="Product 01" /></a>
@@ -224,57 +242,62 @@
         </div> <!-- END of content -->
         <div class="cleaner"></div>
     </div> <!-- END of main -->
-    ?>
+    
 
-                <div id="templatemo_footer">
-                    <div class="col col_16">
-                        <h4>Categories</h4>
-                        <ul class="footer_menu">
-                            <li><a href="#">Aenean et dolor diam</a></li>
-                            <li><a href="#">Aenean pulvinar</a></li>
-                            <li><a href="#">Cras bibendum auctor</a></li>
-                            <li><a href="#">Donec sodales bibendum</a></li>
-                        </ul>
-                    </div>
-                    <div class="col col_16">
-                        <h4>Pages</h4>
-                        <ul class="footer_menu">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">Shipping</a></li>
-                            <li><a href="#">Privacy</a></li>
-                        </ul>
-                    </div>
-                    <div class="col col_16">
-                        <h4>Partners</h4>
-                        <ul class="footer_menu">
-                            <li><a rel="nofollow" href="http://www.flashmo.com/">Free Flash Templates</a></li>
-                            <li><a rel="nofollow" href="http://www.templatemo.com/">Free CSS Templates</a></li>
-                            <li><a href="http://www.koflash.com/">Website Gallery</a></li>
-                            <li><a href="http://www.webdesignmo.com/blog/">Web Design Resources</a></li>
-                        </ul>
-                    </div>
-                    <div class="col col_16">
-                        <h4>Social</h4>
-                        <ul class="footer_menu">
-                            <li><a href="#">Twitter</a></li>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Youtube</a></li>
-                            <li><a href="#">LinkedIn</a></li>
-                        </ul>
-                    </div>
-                    <div class="col col_13 no_margin_right">
-                        <h4>About Us</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper quam sit amet turpis rhoncus id venenatis tellus sollicitudin. Fusce ullamcorper, dolor non mollis pulvinar, turpis tortor commodo nisl. Validate <a href="http://validator.w3.org/check?uri=referer"
-                                rel="nofollow"><strong>XHTML</strong></a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><strong>CSS</strong></a>.</p>
-                    </div>
-
-                    <div class="cleaner h40"></div>
-                    <center>
-                        Copyright © 2048 Your Company Name | Designed by <a href="http://www.templatemo.com" rel="nofollow" target="_parent">Free CSS Templates</a>
-                    </center>
+            <div id="templatemo_footer">
+                <div class="col col_16">
+                    <h4>Categories</h4>
+                    <ul class="footer_menu">
+                        <li><a href="#">Aenean et dolor diam</a></li>
+                        <li><a href="#">Aenean pulvinar</a></li>
+                        <li><a href="#">Cras bibendum auctor</a></li>
+                        <li><a href="#">Donec sodales bibendum</a></li>
+                    </ul>
                 </div>
-                <!-- END of footer -->
+                <div class="col col_16">
+                    <h4>Pages</h4>
+                    <ul class="footer_menu">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Privacy</a></li>
+                    </ul>
+                </div>
+                <div class="col col_16">
+                    <h4>Partners</h4>
+                    <ul class="footer_menu">
+                        <li><a rel="nofollow" href="http://www.flashmo.com/">Free Flash Templates</a></li>
+                        <li><a rel="nofollow" href="http://www.templatemo.com/">Free CSS Templates</a></li>
+                        <li><a href="http://www.koflash.com/">Website Gallery</a></li>
+                        <li><a href="http://www.webdesignmo.com/blog/">Web Design Resources</a></li>
+                    </ul>
+                </div>
+                <div class="col col_16">
+                    <h4>Social</h4>
+                    <ul class="footer_menu">
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Youtube</a></li>
+                        <li><a href="#">LinkedIn</a></li>
+                    </ul>
+                </div>
+                <div class="col col_13 no_margin_right">
+                    <h4>About Us</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper quam sit amet turpis
+                        rhoncus id venenatis tellus sollicitudin. Fusce ullamcorper, dolor non mollis pulvinar, turpis
+                        tortor commodo nisl. Validate <a href="http://validator.w3.org/check?uri=referer"
+                            rel="nofollow"><strong>XHTML</strong></a> &amp; <a
+                            href="http://jigsaw.w3.org/css-validator/check/referer"
+                            rel="nofollow"><strong>CSS</strong></a>.</p>
+                </div>
+
+                <div class="cleaner h40"></div>
+                <center>
+                    Copyright © 2048 Your Company Name | Designed by <a href="http://www.templatemo.com" rel="nofollow"
+                        target="_parent">Free CSS Templates</a>
+                </center>
+            </div>
+            <!-- END of footer -->
 
         </div>
 

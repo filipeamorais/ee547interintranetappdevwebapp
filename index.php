e3e117bed27b82350800668aa9c341e2f1eb8856
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-28 21:58:06 -0500

    removed: more unused js at the beginning.

--- index.php
+++ index.php
@@ -12,44 +12,7 @@
     <script type="text/javascript" src="js/ddsmoothmenu.js">
     </script>
 
-    <!-- <script type="text/javascript">
-        ddsmoothmenu.init({
-            mainmenuid: "templatemo_menu", //menu DIV id
-            orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
-            classname: 'ddsmoothmenu', //class added to menu's outer DIV
-            //customtheme: ["#1c5a80", "#18374a"],
-            contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
-        })
-    </script> -->
-
     <link rel="stylesheet" type="text/css" href="css/styles.css" />
-    <script language="javascript" type="text/javascript" src="scripts/mootools-1.2.1-core.js"></script>
-    <script language="javascript" type="text/javascript" src="scripts/mootools-1.2-more.js"></script>
-    <script language="javascript" type="text/javascript" src="scripts/slideitmoo-1.1.js"></script>
-    <script language="javascript" type="text/javascript">
-        window.addEvents({
-            'domready': function () {
-                /* thumbnails example , div containers */
-                new SlideItMoo({
-                    overallContainer: 'SlideItMoo_outer',
-                    elementScrolled: 'SlideItMoo_inner',
-                    thumbsContainer: 'SlideItMoo_items',
-                    itemsVisible: 5,
-                    elemsSlide: 2,
-                    duration: 200,
-                    itemsSelector: '.SlideItMoo_element',
-                    itemWidth: 171,
-                    showControls: 1
-                });
-            },
-
-        });
-
-        function clearText(field) {
-            if (field.defaultValue == field.value) field.value = '';
-            else if (field.value == '') field.value = field.defaultValue;
-        }
-    </script>
     <?php
     include 'rootsdb.inc';
     $connection = mysqli_connect($host, $user, $password, $database);

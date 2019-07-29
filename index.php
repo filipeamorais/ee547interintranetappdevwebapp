1f96897d041abb52251a161747af601b239e634a
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-28 22:06:10 -0500

    update: formated the header code and changed some variables names.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Roots Coffee - The online coffee shop store</title>
@@ -7,54 +8,54 @@
     <meta name="description" content="Roots Coffee - The online coffee shop store provided by filipeam" />
     <link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
 
     <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/ddsmoothmenu.js">
-    </script>
+    <script type="text/javascript" src="js/ddsmoothmenu.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="css/styles.css" />
     <?php
     include 'rootsdb.inc';
-    $connection = mysqli_connect($host, $user, $password, $database);
-            
-    if (mysqli_connect_errno()) {
-      die(mysqli_connect_error());    // not production error handling
-    }
     session_start();
-    if (isset($_POST['prod_no']) && $_POST['prod_no']!=""){
+    $connection = mysqli_connect($host, $user, $password, $database);
+
+    if (mysqli_connect_errno()) {
+        die(mysqli_connect_error());    // not production error handling
+    }
+
+    if (isset($_POST['prod_no']) && $_POST['prod_no'] != "") {
         $prod_no = $_POST['prod_no'];
-        $result = mysqli_query($connection,"SELECT * FROM `tblproduct` WHERE `prod_no`='$prod_no'");
-        $row = mysqli_fetch_assoc($result);
-        $name = $row['prod_name'];
-        $code = $row['prod_no'];
-        $price = $row['prod_price'];
-         
+        $resultAddedProduct = mysqli_query($connection, "SELECT * FROM `tblproduct` WHERE `prod_no`='$prod_no'");
+        $addedProductRow = mysqli_fetch_assoc($resultAllTheProducts);
+        $nameProd = $addedProductRow['prod_name'];
+        $codeProd = $addedProductRow['prod_no'];
+        $priceProd = $addedProductRow['prod_price'];
+
         $cartArray = array(
-         $code=>array(
-         'name'=>$name,
-         'code'=>$code,
-         'price'=>$price,
-         'quantity'=>1)
+            $code => array(
+                'name' => $nameProd,
+                'code' => $codeProd,
+                'price' => $priceProd,
+                'quantity' => 1
+            )
         );
-         
-        if(empty($_SESSION["shopping_cart"])) {
+
+        if (empty($_SESSION["shopping_cart"])) {
             $_SESSION["shopping_cart"] = $cartArray;
             $status = "<div class='box'>Product is added to your cart!</div>";
-        }else{
+        } else {
             $array_keys = array_keys($_SESSION["shopping_cart"]);
-            if(in_array($code,$array_keys)) {
-         $status = "<div class='box' style='color:red;'>
-         Product is already added to your cart!</div>"; 
+            if (in_array($code, $array_keys)) {
+                $status = "<div class='box' style='color:red;'>
+         Product is already added to your cart!</div>";
             } else {
-            $_SESSION["shopping_cart"] = array_merge(
-            $_SESSION["shopping_cart"],
-            $cartArray
-            );
-            $status = "<div class='box'>Product is added to your cart!</div>";
-         }
-         
-         }
+                $_SESSION["shopping_cart"] = array_merge(
+                    $_SESSION["shopping_cart"],
+                    $cartArray
+                );
+                $status = "<div class='box'>Product is added to your cart!</div>";
+            }
         }
+    }
     ?>
 </head>
 
@@ -85,10 +86,8 @@
 
                 <div id="templatemo_search">
                     <form action="#" method="get">
-                        <input type="text" value="Search" name="keyword" id="keyword" title="keyword"
-                            onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
-                        <input type="submit" name="Search" value="" alt="Search" id="searchbutton" title="Search"
-                            class="sub_btn" />
+                        <input type="text" value="Search" name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
+                        <input type="submit" name="Search" value="" alt="Search" id="searchbutton" title="Search" class="sub_btn" />
                     </form>
                 </div>
             </div>
@@ -154,16 +153,12 @@
                     <li><a href="#">Tincidunt leo</a></li>
                     <li><a href="#">Vestibulum blandit</a></li>
                 </ul>
-                <h3><a class="sidebar_title" href="http://www.mystockvector.com" title="stock vector"
-                        target="_blank">Newsletter</a></h3>
+                <h3><a class="sidebar_title" href="http://www.mystockvector.com" title="stock vector" target="_blank">Newsletter</a></h3>
                 <p>Praesent aliquam mi id tellus pretium pulvinar in vel ligula.</p>
                 <div id="newsletter">
                     <form action="#" method="get">
-                        <input type="text" value="Subscribe" name="email_newsletter" id="email_newsletter"
-                            title="email_newsletter" onfocus="clearText(this)" onblur="clearText(this)"
-                            class="txt_field" />
-                        <input type="submit" name="subscribe" value="Subscribe" alt="Subscribe" id="subscribebtn"
-                            title="Subscribe" class="subscribebtn" />
+                        <input type="text" value="Subscribe" name="email_newsletter" id="email_newsletter" title="email_newsletter" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
+                        <input type="submit" name="subscribe" value="Subscribe" alt="Subscribe" id="subscribebtn" title="Subscribe" class="subscribebtn" />
                     </form>
                     <div class="cleaner"></div>
                 </div>
@@ -171,95 +166,91 @@
             <!-- END of sidebar -->
 
             <?php
-            
+
             include 'rootsdb.inc';
 
             $connection = mysqli_connect($host, $user, $password, $database);
-            
+
             if (mysqli_connect_errno()) {
-              die(mysqli_connect_error());    // not production error handling
+                die(mysqli_connect_error());    // not production error handling
             }
 
             $sqlSelectAllTheProducts = "SELECT * FROM tblproduct";
             $resultAllTheProducts = mysqli_query($connection, $sqlSelectAllTheProducts);
-            while ($allTheProductsRow = mysqli_fetch_assoc($resultAllTheProducts)){
-            
+            while ($allTheProductsRow = mysqli_fetch_assoc($resultAllTheProducts)) {
+
                 echo '<form method="post" action="" id="form1">
                 <div id="content">';
-            echo '<div class="col col_14 product_gallery">';
-            echo '<a href="productdetail.php?prod_no='.$allTheProductsRow["prod_no"].'"><img src="images/product/'.$allTheProductsRow["prod_no"].'.jpg" alt="Product 01"></a>';
-            echo '<h3>'.$allTheProductsRow["prod_name"].'</h3>';
-            echo "<p class=\"product_price\">".$allTheProductsRow['prod_price']."</p>
-            <input type='hidden' name='prod_no' value=".$allTheProductsRow['prod_no']." />";
-            echo ' <button type="submit" class="buy">Buy Now</button>
+                echo '<div class="col col_14 product_gallery">';
+                echo '<a href="productdetail.php?prod_no=' . $allTheProductsRow["prod_no"] . '"><img src="images/product/' . $allTheProductsRow["prod_no"] . '.jpg" alt="Product 01"></a>';
+                echo '<h3>' . $allTheProductsRow["prod_name"] . '</h3>';
+                echo "<p class=\"product_price\">" . $allTheProductsRow['prod_price'] . "</p>
+            <input type='hidden' name='prod_no' value=" . $allTheProductsRow['prod_no'] . " />";
+                echo ' <button type="submit" class="buy">Buy Now</button>
             <a href="shoppingcart.php" onclick="document.getElementById("form1").submit(); return false;" class="add_to_cart">Add to Cart</a>';
-            echo "</div></form>";
+                echo "</div></form>";
             }
             ?>
         </div> <!-- END of content -->
         <div class="cleaner"></div>
     </div> <!-- END of main -->
-    
 
-            <div id="templatemo_footer">
-                <div class="col col_16">
-                    <h4>Categories</h4>
-                    <ul class="footer_menu">
-                        <li><a href="#">Aenean et dolor diam</a></li>
-                        <li><a href="#">Aenean pulvinar</a></li>
-                        <li><a href="#">Cras bibendum auctor</a></li>
-                        <li><a href="#">Donec sodales bibendum</a></li>
-                    </ul>
-                </div>
-                <div class="col col_16">
-                    <h4>Pages</h4>
-                    <ul class="footer_menu">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Shipping</a></li>
-                        <li><a href="#">Privacy</a></li>
-                    </ul>
-                </div>
-                <div class="col col_16">
-                    <h4>Partners</h4>
-                    <ul class="footer_menu">
-                        <li><a rel="nofollow" href="http://www.flashmo.com/">Free Flash Templates</a></li>
-                        <li><a rel="nofollow" href="http://www.templatemo.com/">Free CSS Templates</a></li>
-                        <li><a href="http://www.koflash.com/">Website Gallery</a></li>
-                        <li><a href="http://www.webdesignmo.com/blog/">Web Design Resources</a></li>
-                    </ul>
-                </div>
-                <div class="col col_16">
-                    <h4>Social</h4>
-                    <ul class="footer_menu">
-                        <li><a href="#">Twitter</a></li>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Youtube</a></li>
-                        <li><a href="#">LinkedIn</a></li>
-                    </ul>
-                </div>
-                <div class="col col_13 no_margin_right">
-                    <h4>About Us</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper quam sit amet turpis
-                        rhoncus id venenatis tellus sollicitudin. Fusce ullamcorper, dolor non mollis pulvinar, turpis
-                        tortor commodo nisl. Validate <a href="http://validator.w3.org/check?uri=referer"
-                            rel="nofollow"><strong>XHTML</strong></a> &amp; <a
-                            href="http://jigsaw.w3.org/css-validator/check/referer"
-                            rel="nofollow"><strong>CSS</strong></a>.</p>
-                </div>
 
-                <div class="cleaner h40"></div>
-                <center>
-                    Copyright © 2048 Your Company Name | Designed by <a href="http://www.templatemo.com" rel="nofollow"
-                        target="_parent">Free CSS Templates</a>
-                </center>
-            </div>
-            <!-- END of footer -->
-
+    <div id="templatemo_footer">
+        <div class="col col_16">
+            <h4>Categories</h4>
+            <ul class="footer_menu">
+                <li><a href="#">Aenean et dolor diam</a></li>
+                <li><a href="#">Aenean pulvinar</a></li>
+                <li><a href="#">Cras bibendum auctor</a></li>
+                <li><a href="#">Donec sodales bibendum</a></li>
+            </ul>
+        </div>
+        <div class="col col_16">
+            <h4>Pages</h4>
+            <ul class="footer_menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Shipping</a></li>
+                <li><a href="#">Privacy</a></li>
+            </ul>
+        </div>
+        <div class="col col_16">
+            <h4>Partners</h4>
+            <ul class="footer_menu">
+                <li><a rel="nofollow" href="http://www.flashmo.com/">Free Flash Templates</a></li>
+                <li><a rel="nofollow" href="http://www.templatemo.com/">Free CSS Templates</a></li>
+                <li><a href="http://www.koflash.com/">Website Gallery</a></li>
+                <li><a href="http://www.webdesignmo.com/blog/">Web Design Resources</a></li>
+            </ul>
+        </div>
+        <div class="col col_16">
+            <h4>Social</h4>
+            <ul class="footer_menu">
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Youtube</a></li>
+                <li><a href="#">LinkedIn</a></li>
+            </ul>
+        </div>
+        <div class="col col_13 no_margin_right">
+            <h4>About Us</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper quam sit amet turpis
+                rhoncus id venenatis tellus sollicitudin. Fusce ullamcorper, dolor non mollis pulvinar, turpis
+                tortor commodo nisl. Validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow"><strong>XHTML</strong></a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><strong>CSS</strong></a>.</p>
         </div>
 
+        <div class="cleaner h40"></div>
+        <center>
+            Copyright © 2048 Your Company Name | Designed by <a href="http://www.templatemo.com" rel="nofollow" target="_parent">Free CSS Templates</a>
+        </center>
+    </div>
+    <!-- END of footer -->
 
-        <script type='text/javascript' src='js/logging.js'></script>
+    </div>
+
+
+    <script type='text/javascript' src='js/logging.js'></script>
 </body>
 
 </html>
